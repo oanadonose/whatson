b8e09f71be0fbea833867fa51076befe378b30f6
Oana Donose <donoseoana@gmail.com>
2019-08-29 16:59:09 +0100

    initial commit, get cinemas and movies basic functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App] constructor");
+    this.state = {
+      postcode: "LE50QA", //default. change this with user input
+      nearbyCinemas: []
+    }
+  }
+
+  //all cinemas objects nearby
+  getNearbyCinemas = () => {
+    fetch(`https://api.cinelist.co.uk/search/cinemas/postcode/LE50QA`)
+    .then(data => data.json())
+    .then(data => {
+      this.setState({
+        nearbyCinemas: data
+      });
+    });
+  }
+
+
+  getNearbyMovies = () => {
+    var cinemaIds = [];
+    this.state.nearbyCinemas.cinemas.forEach(element => {
+      cinemaIds.push(element.id);
+    });
+    fetch(`https://api.cinelist.co.uk/get/times/many/` + cinemaIds.join(","))
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+    })
+  }
+
+  
+
+  componentDidMount() {
+    this.getNearbyCinemas();
+   
+  }
+
+  componentDidUpdate() {
+    this.getNearbyMovies();
+  }
+  render () {
+    //console.log(this.state);
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
