7786a8fa056409a8b1c87f54bfd9c19dd3caf4e2
Oana Donose <donoseoana@gmail.com>
2019-09-03 11:39:39 +0100

    made one overpowered fetch for everything can get from cinelist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//tmdbapikey: 2ca7f95a77ff82974f6799d4ad26b7c8
+//tmdb: https://api.themoviedb.org/3/movie/550?api_key=2ca7f95a77ff82974f6799d4ad26b7c8
 
 class App extends Component {
   constructor(props) {
@@ -8,44 +10,62 @@ class App extends Component {
     console.log("[App] constructor");
     this.state = {
       postcode: "LE50QA", //default. change this with user input
-      nearbyCinemas: []
+      nearbyCinemas: [],
+      nearbyMovies: [],
+      movies: []
     }
   }
 
   //all cinemas objects nearby
-  getNearbyCinemas = () => {
+  getNearbyMovies = () => {
     fetch(`https://api.cinelist.co.uk/search/cinemas/postcode/LE50QA`)
     .then(data => data.json())
     .then(data => {
-      this.setState({
+      this.setState((state) => ({
         nearbyCinemas: data
-      });
-    });
-  }
-
-
-  getNearbyMovies = () => {
-    var cinemaIds = [];
-    this.state.nearbyCinemas.cinemas.forEach(element => {
-      cinemaIds.push(element.id);
-    });
-    fetch(`https://api.cinelist.co.uk/get/times/many/` + cinemaIds.join(","))
-    .then(data => data.json())
-    .then(data => {
-      console.log(data);
+      }));
     })
-  }
+    .then(data => {
+      var cinemaIds = [];
+      this.state.nearbyCinemas.cinemas.forEach(element => {
+        cinemaIds.push(element.id);
+      });
+      fetch(`https://api.cinelist.co.uk/get/times/many/` + cinemaIds.join(","))
+      .then(data => data.json())
+      .then(data => {
+        this.setState((state) => ({
+          nearbyMovies: data.results
+        }));
+        data.results.forEach(cinema => {
+          console.log(cinema);
+          cinema.listings.forEach(listing => {
+            if(this.state.movies.includes(listing.title)){
+              //do nothing. no duplicates
+            }
+            else {
+              this.setState((state)=> ({
+                movies: [...this.state.movies, listing.title]
+              }))
+            }
+          })
+          //cinema.forEach()
+          //if(this.state.movies.includes())
+        })
+    });
+  })
+}
 
   
 
   componentDidMount() {
-    this.getNearbyCinemas();
-   
+    this.getNearbyMovies();   
   }
 
   componentDidUpdate() {
-    this.getNearbyMovies();
+
   }
+
+
   render () {
     //console.log(this.state);
     return (
